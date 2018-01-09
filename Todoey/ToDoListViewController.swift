ac7b0872b54899ddd77fe9afbe3c6baec2e7ced2
Toan Nguyen <toannguyen@toans-mbp>
2018-01-09 13:14:32 +0100

    Data persisted using UserDefaults method.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,10 +12,17 @@ class ToDoListViewController: UITableViewController {
     
     //TODO: Declare Properties
     
+    //UserDefaults: An interface to the user’s defaults database, where you store key-value pairs persistently across launches of your app, allowing an app to customize its behavior to match a user’s preferences.
+
+    let userDefault = UserDefaults.standard
     var itemArray = ["Find Mike", "Buy Eggs", "Eat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Load saved .plist
+        if let items = userDefault.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -27,6 +34,7 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
+//      cell.textLabel?.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
         return cell
     }
     
@@ -51,6 +59,8 @@ class ToDoListViewController: UITableViewController {
         //"Declare" Button and Add Textfield Input to List
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textfield.text!)
+            //Save itemArray to .plist with Key
+            self.userDefault.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
             print(textfield.text!)
         }
