566cb007acac56e6340bf0bcbb7bd26e20677652
Toan Nguyen <toannguyen@toans-mbp>
2018-01-13 20:01:59 +0100

    Delete Commented Code

--- Todoey/Controller/ToDoListViewController.swift
+++ Todoey/Controller/ToDoListViewController.swift
@@ -13,17 +13,13 @@ class ToDoListViewController: UITableViewController {
     
     //TODO: Declare Properties
     
-    //UserDefaults: An interface to the user’s defaults database, where you store key-value pairs persistently across launches of your app, allowing an app to customize its behavior to match a user’s preferences.
-
-    let userDefault = UserDefaults.standard
     var itemArray = [Item]()
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(dataFilePath)
+        
+        print(dataFilePath!)
         // Do any additional setup after loading the view, typically from a nib.
         
         loadItems()
@@ -40,18 +36,10 @@ class ToDoListViewController: UITableViewController {
         let item = itemArray[indexPath.row]
         cell.textLabel?.text = item.title
 //      cell.textLabel?.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
-        /*Shorter Way by Using Ternary Operator for:
-        if item.done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }*/
         //Ternary Operator: value = condition ? valueOfTrue : valueOfFalse
         cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
-    
-    
     
     //MARK: TableView Delegate Methods / DidSelectRow
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -75,11 +63,13 @@ class ToDoListViewController: UITableViewController {
             self.saveItems()
             print(textfield.text!)
         }
+        
         //Textfield
         alert.addTextField { (alertTextfield) in
             alertTextfield.placeholder = "Create new item"
             textfield = alertTextfield
         }
+        
         //Add Button to PopUp
         alert.addAction(action)
         //"Init" PopUp with Button
@@ -97,7 +87,7 @@ class ToDoListViewController: UITableViewController {
         } catch {
             print("Error encoding items \(error)")
         }
-        self.tableView.reloadData()
+        tableView.reloadData()
     }
     
     //Load items
@@ -105,11 +95,12 @@ class ToDoListViewController: UITableViewController {
         if let data = try? Data(contentsOf: dataFilePath!) {
             let decoder = PropertyListDecoder()
             do {
-            itemArray = try decoder.decode([Item].self, from: data)
+                itemArray = try decoder.decode([Item].self, from: data)
             } catch {
                 print("Error decoding items: \(error)")
             }
         }
     }
+    
 }
 
